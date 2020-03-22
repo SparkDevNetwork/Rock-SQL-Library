c667c77a6a3ea3413b8da3ac2d5a320c9716e88e
Jon Edmiston <jon@sparkdevnetwork.org>
2020-03-22 15:53:43 -0700

    Updated Page Load Times SQL

--- Interactions/PageViews/Page Load Times.sql
+++ Interactions/PageViews/Page Load Times.sql
@@ -6,23 +6,34 @@
 -- ...
 -- Parameters:
 --   @PageId - Id of the page you want timings from. 
---   @MaxRows - The maximum number of rows to return
+--   @MaxRows - The maximum number of rows to return.
+--   @DaysBack - The number of days back to look.
 -- =====================================================================================================
 
-
-DECLARE @PageId int = 1
+DECLARE @PageId int = 1247
 DECLARE @MaxRows int = 1000
+DECLARE @DaysBack int = 1
+
+-------------------------------------------------------------------------------------------------------
+
+SET @DaysBack = @DaysBack * -1
+DECLARE @StartDate datetime = (SELECT DATEADD (day , @DaysBack , GETDATE() ) )
+
 
 SELECT
-    TOP(@MaxRows) [InteractionTimeToServe], [InteractionDateTime], p.[NickName], p.[LastName]
-   
+    TOP (@MaxRows) 
+    [InteractionTimeToServe], 
+    [InteractionDateTime], 
+    p.[NickName], 
+    p.[LastName]
 FROM
     [Interaction] i
     INNER JOIN [InteractionComponent] ic ON ic.[Id] = i.[InteractionComponentId]
     LEFT OUTER JOIN [PersonAlias] pa ON pa.[Id] = i.[PersonAliasId]
     LEFT OUTER JOIN [Person] p ON p.[Id] = pa.[PersonId]
 WHERE
-    i.[EntityId] = @PageId AND [InteractionTimeToServe] IS NOT NULL
-    AND [InteractionDateTime] > '3/20/2020'
+    ic.[EntityId] = @PageId 
+    AND [InteractionTimeToServe] IS NOT NULL
+    AND [InteractionDateTime] > @StartDate
 ORDER BY [InteractionTimeToServe] DESC
 --ORDER BY [InteractionDateTime] DESC
