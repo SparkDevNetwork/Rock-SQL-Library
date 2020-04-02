456b23e8c63446d6dcf34d6f5db559630391b28b
Jon Edmiston <jon@sparkdevnetwork.org>
2020-04-01 23:28:51 -0700

    update note on group sync stats SQL

--- Groups/Group Sync Information.sql
+++ Groups/Group Sync Information.sql
@@ -3,6 +3,7 @@
 -- Create Date: 4/1/2020
 -- Description: Displays a list of the all the group syncs configured for the system and how long 
 --              each data view takes to run.
+--              This requires Rock v11.
 --              Note: Time is in millseconds.
 --
 -- Change History:
