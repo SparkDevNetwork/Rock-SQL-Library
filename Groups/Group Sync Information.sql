eada0f526fa78f487b6a4e0eedbff6909d15e25b
Jon Edmiston <jon@sparkdevnetwork.org>
2020-04-01 22:17:56 -0700

    Added SQL to report on Group Sync data to help find which syncs are taking a long time

---
+++ Groups/Group Sync Information.sql
@@ -0,0 +1,18 @@
+-- =====================================================================================================
+-- Author:      Jon Edmiston
+-- Create Date: 4/1/2020
+-- Description: Displays a list of the all the group syncs configured for the system and how long 
+--              each data view takes to run.
+--              Note: Time is in millseconds.
+--
+-- Change History:
+--   
+-- =====================================================================================================
+
+SELECT
+    g.[Name] AS [Group Name], gtr.[Name] AS [Role], dv.[Name] AS [Data View], dv.[TimeToRunMS]
+FROM
+    [GroupSync] gs
+    INNER JOIN [Group] g ON g.[Id] = gs.[GroupId]
+    INNER JOIN [GroupTypeRole] gtr ON gtr.[Id] = gs.[GroupTypeRoleId]
+    INNER JOIN [DataView] dv ON dv.[Id] = gs.[SyncDataViewId]
