89114f7c2534e8a2491dd1e3789c13d7ab73a115
Jon Edmiston <jon@sparkdevnetwork.org>
2020-04-23 21:33:22 -0700

    + Added statement to get count of interactions by component for a given channel

---
+++ Interactions/Get Count of Interactions by Component for a Given Channel.sql
@@ -0,0 +1,47 @@
+-- =====================================================================================================
+-- Author:      Jon Edmiston
+-- Create Date: 4/23/2020
+-- Description: Displays the number of interactions for each component of a channel for a number of days back
+--
+-- Change History:
+--   
+-- =====================================================================================================
+
+DECLARE @InteractionChannelId int = 23   -- Id of the Interaction Channel you want to filter on
+DECLARE @DaysBack int = -1               -- The number of days back to look. Use -1 if you want all
+
+-------------------------------------------------------------------------------------------------------
+
+SET @DaysBack = @DaysBack * -1
+DECLARE @StartDate datetime = (SELECT DATEADD (day , @DaysBack , GETDATE() ) )
+
+------------------------------
+
+-- Using two different selects as checking the InteractionDateTime is slower and we may not care about it
+IF ( @DaysBack = -1 )
+    BEGIN
+        SELECT  
+            ic.[Id]
+            , ic.[Name]
+            , (SELECT COUNT(*) FROM [Interaction] i WHERE i.[InteractionComponentId] = ic.[Id]  ) AS [InteractionCount]
+        FROM [InteractionComponent] ic 
+        WHERE 
+            ic.[InteractionChannelId] = @InteractionChannelId
+        ORDER BY ic.[Name]
+    END;
+
+ELSE
+    BEGIN
+        SELECT  
+            ic.[Id]
+            , ic.[Name]
+            , (SELECT COUNT(*) FROM [Interaction] i WHERE i.[InteractionComponentId] = ic.[Id] AND [InteractionDateTime] >= @StartDate  ) AS [InteractionCount]
+        FROM [InteractionComponent] ic 
+        WHERE 
+            ic.[InteractionChannelId] = @InteractionChannelId
+        ORDER BY ic.[Name]
+    END; 
+
+
+
+    
